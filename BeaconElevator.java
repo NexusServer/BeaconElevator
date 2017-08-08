fa2c504faa7d91ec881fa720aa969232b788a27d
사본 <heejae5599@gmail.com>
2017-08-08 10:23:04 +0900

    Update BeaconElevator.java

--- BeaconElevator.java
+++ BeaconElevator.java
@@ -9,14 +9,15 @@ import cn.nukkit.math.Vector3;
 import cn.nukkit.event.player.PlayerToggleSneakEvent;
 import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.event.entity.EntityDamageEvent.DamageCause;
 import cn.nukkit.event.player.PlayerKickEvent;
 import cn.nukkit.utils.Config;
 import cn.nukkit.block.Block;
-import cn.nukkit.level.Level;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 
-import cn.nukkit.util.ArrayList;
+import java.util.ArrayList;
+
 
 
 public class BeaconElevator extends PluginBase implements Listener{
@@ -26,32 +27,33 @@ public class BeaconElevator extends PluginBase implements Listener{
 	 * x;y;z;world : height
 	 */
 	
-	 List<Player> ff=new List<Player>(); //출발지점을 지정하는 플레이어를 저장
-	 List<Player> sf=new List<Player>(); //도착지점을 지정하는 플레이어를 저장
-	 List<Player> del=new List<Player>(); //삭제하는 플레이어를 저장
-	 List<Player> players=new List<Player>(); //엘리베이터에 타는 사람을 저장
+	 ArrayList<Player> ff=new ArrayList<Player>(); //출발지점을 지정하는 플레이어를 저장
+	 ArrayList<Player> sf=new ArrayList<Player>(); //도착지점을 지정하는 플레이어를 저장
+	 ArrayList<Player> del=new ArrayList<Player>(); //삭제하는 플레이어를 저장
+	 ArrayList<Player> players=new ArrayList<Player>(); //엘리베이터에 타는 사람을 저장
 	 public Config config; 
 	
 	 public void onEnable(){
-	 	  this.getServer().getPluginManager.registerEvents(this,this);
+	 	  this.getServer().getPluginManager().registerEvents(this,this);
 	 	  this.getDataFolder().mkdirs();
-   	  this.config=new Config(this.getDataFolder()+"/config.yml",Config.YAML);
+	 	  this.config=new Config(this.getDataFolder()+"/config.yml",Config.YAML);
 	 }
 	 
 	 public void onDisable(){
 	 	 this.config.save();
 	 }
 	 
-	 @Override
-   public void onSnick(PlayerToggleSneakEvent ev){
+	 public void onSnick(PlayerToggleSneakEvent ev){
    	  Player player=ev.getPlayer();
    	  int x=player.getFloorX();
    	  int y=player.getFloorY()-1;
    	  int z=player.getFloorZ();
-   	  String folder=level.getFolderName();
+   	  Level level=player.getLevel();
+   	  String world=level.getFolderName();
    	  String location=String.valueOf(x)+String.valueOf(y)+String.valueOf(z)+world;
+   	  int height=Integer.parseInt(this.config.get(location).toString());
    	  String destin=String.valueOf(x)+String.valueOf(y-height)+String.valueOf(z)+world;
-   	  int height=Integer.parseInt(this.config.get(location));
+   	  
    	  if(player.getLevel().getBlockIdAt(x,y,z)==Block.BEACON&&this.config.exists(location)){
    	  	 for(int e=0;e<=height;e++){
    	  	 	  player.getLevel().setBlock(new Vector3(x,y+height-1,z),Block.get(0,0),true,false);
@@ -59,7 +61,7 @@ public class BeaconElevator extends PluginBase implements Listener{
    	  	 	  players.add(player);
    	  	 }
    	  	 player.teleport(new Vector3(x,y+height+0.63,z));
-   	  	 player.remove(player);
+   	  	 players.remove(player);
    	  	 player.getLevel().setBlock(new Vector3(x,y-1,z),Block.get(Block.GLASS,0),true,false);
    	  }
    	  if(this.config.exists(destin)){
@@ -69,15 +71,15 @@ public class BeaconElevator extends PluginBase implements Listener{
    	  	 	  players.add(player);
    	  	 }
    	  	 player.teleport(new Vector3(x,y+height-0.63,z));
-   	  	 player.remove(player);
+   	  	 players.remove(player);
    	  	 player.getLevel().setBlock(new Vector3(x,y+height-1,z),Block.get(0,0),true,false);
    	  }
    }
    
    public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args){
    	  if(cmd.getName().equals("엘베")||cmd.getName().equalsIgnoreCase("el")){
-   	  	 if(!sender instanceof Player){
-   	  	  	 sender.sendMessage("플레이어만 사용 가능합니다.")
+   	  	 if(!(sender instanceof Player)){
+   	  	  	 sender.sendMessage("플레이어만 사용 가능합니다.");
    	  	  	 return true;
    	  	 }
    	  	 Player player=(Player)sender;
@@ -101,87 +103,88 @@ public class BeaconElevator extends PluginBase implements Listener{
    	  	 	  return true;
    	  	 }
    	  }
+	return true;
    }
    
-   @Override
-   public void preventFallenDamage(EntityDamageEvent ev){
-   	  if(event.getCause()==EntityDamageEvent.CAUSE_FALL){
-   	  	 if(!ev.getEntity() instanceof Player){
+   public void preventFallenDamage(EntityDamageEvent event){
+   	  if(event.getCause()==DamageCause.FALL){
+   	  	 if(!(event.getEntity() instanceof Player)){
    	  	 	  return;
    	  	 }
-   	  	 Player player=(Player)ev.getEntity();
-   	  	 if(players.exists(player)){
+   	  	 Player player=(Player)event.getEntity();
+   	  	 if(players.contains(player)){
    	  	 	  event.setCancelled(true);
    	  	 }
    	  }
    }
    
-   @Override
    public void preventFlyKick(PlayerKickEvent ev){
-   	  if(players.exists(player)){
+	  Player player=ev.getPlayer();
+   	  if(players.contains(player)){
    	  	 if(ev.getReason()=="Flying is not enabled on this server"){
-   	  	 	  event.setCancelled(true);
+   	  	 	  ev.setCancelled(true);
    	  	 }
    	  }
    }
    
-   @Override
-   public void onTouch(PlayerInteractEvent ev){
-   	  Player player=ev.getPlayer();
+   public void onTouch(PlayerInteractEvent event){
+   	  Player player=event.getPlayer();
    	  int x=event.getBlock().getFloorX();
    	  int y=event.getBlock().getFloorY();
    	  int z=event.getBlock().getFloorZ();
-   	  String folder=level.getFolderName();
+   	  Level level=player.getLevel();
+   	  String first=null;
+   	  int first_y=0;
+   	  String world=level.getFolderName();
    	  String location=String.valueOf(x)+String.valueOf(y)+String.valueOf(z)+world;
-   	  String first;
-   	  int first_y;
-   	  int height=Integer.parseInt(this.config.get(location));
+   	  int height=Integer.parseInt(this.config.get(location).toString());
    	  if(ff.contains(player)){
-   	  	 if(!ev.getBlock().getId()==Block.BEACON){
+   	  	 if(!(event.getBlock().getId()==Block.BEACON)){
    	  	 	  player.sendTip("§e[엘레베이터] 신호기를 터치해주세요.");
-   	  	 	  ev.setCancelled(true);
+   	  	 	  event.setCancelled(true);
    	  	 	  return;
    	  	 }
    	  	 if(this.config.exists(location)){
-   	  	 	  player.sendTip("§[엘레베이터] 이미 존재하는 엘레베이터 입니다.");
-   	  	 	  ev.setCancelled(true);
+   	  	 	  player.sendTip("§e[엘레베이터] 이미 존재하는 엘레베이터 입니다.");
+   	  	 	  event.setCancelled(true);
    	  	 	  return;
    	  	 }
    	  	 first=location;
    	  	 first_y=y;
-   	  	 player.sendTip("§6[엘레베이터] 도착지점을 터치해주세요.")
-   	  	 ev.setCancelled(true);
+   	  	 player.sendTip("§6[엘레베이터] 도착지점을 터치해주세요.");
+   	  	 event.setCancelled(true);
    	  	 ff.remove(player);
    	  	 return;
    	  }
    	  if(sf.contains(player)){
    	  	 if(first_y!=y){
    	  	 	  player.sendTip("§e[엘레베이터] y좌표가 다릅니다.");
-   	  	 	  ev.setCancelled(true);
+   	  	 	  event.setCancelled(true);
    	  	 	  return;
    	  	 }
    	  	 this.config.set(first,height);
-   	  	 player.sendTip("§6[엘레베이터] 엘레비에터가 생성되었습니다.")
-   	  	 ev.setCancelled(true);
+   	  	 player.sendTip("§6[엘레베이터] 엘레비에터가 생성되었습니다.");
+   	  	 event.setCancelled(true);
    	  	 sf.remove(player);
-   	  	 first_y=null;
-   	  	 first=null;
+   	  	 first_y=0;
+   	  	 first="";
    	  	 return;
    	  }
    	  if(del.contains(player)){
-   	  	 if(!ev.getBlock().getId()==Block.BEACON){
+   	  	 if(!(event.getBlock().getId()==Block.BEACON)){
    	  	 	  player.sendTip("§e[엘레베이터] 신호기를 터치해주세요.");
-   	  	 	  ev.setCancelled(true);
+   	  	 	  event.setCancelled(true);
    	  	 	  return;
    	  	 }
    	  	 if(this.config.exists(location)){
-   	  	 	  player.sendTip("§[엘레베이터] 엘리베이터를 삭제합니다.");
+   	  	 	  player.sendTip("§6[엘레베이터] 엘리베이터를 삭제합니다.");
    	  	 	  this.config.remove(location);
-   	  	 	  ev.setCancelled(true);
+   	  	 	  event.setCancelled(true);
    	  	 	  return;
    	  	 }
-   	  	 first=null;
-   	  	 first_y=null;
+   	  	 first="";
+   	  	 first_y=0;
+   	  	 
    	  	 ff.remove(player);
    	  	 return;
    	  }
